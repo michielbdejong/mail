599328eae51c180d9b2c7015da3af0890aeeb96f
Michiel de Jong <michiel@unhosted.org>
2014-08-08 16:58:51 +0200

    generate cert during dovecot install

--- install/init.sh
+++ install/init.sh
@@ -5,11 +5,13 @@ sed -i "s/mailuserpass/$MYSQL_PASSWORD/g" /install/dovecot/dovecot-sql.conf.ext
 sed -i "s/mailuserpass/$MYSQL_PASSWORD/g" /install/postfix/*
 sed -i "s/hostname.com/$HOST_NAME/g" /install/init.mysql
 sed -i "s/hostname.com/$HOST_NAME/g" /install/postfix/*
-sed -i "s/hostname.com/$HOST_NAME/g" /install/dovecot/dovecot-openssl.cnf
 echo "mysql-server mysql-server/root_password password $MYSQL_PASSWORD" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $MYSQL_PASSWORD" | debconf-set-selections
 echo "postfix postfix/mailname string $HOST_NAME" | debconf-set-selections
 echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
+echo "dovecot-core dovecot-core/create-ssl-cert boolean true" | debconf-set-selections
+echo "dovecot-core dovecot-core/ssl-cert-name string $HOST_NAME" | debconf-set-selections
+
 apt-get install -y postfix postfix-mysql dovecot-core dovecot-imapd dovecot-pop3d dovecot-lmtpd dovecot-mysql mysql-server
 
 #MySQL
@@ -36,7 +38,4 @@ cp /install/dovecot/dovecot-sql.conf.ext /etc/dovecot/dovecot-sql.conf.ext
 chown -R vmail:dovecot /etc/dovecot
 chmod -R o-rwx /etc/dovecot
 cp /install/dovecot/conf.d/10-master.conf /etc/dovecot/conf.d/10-master.conf
-cp /install/dovecot/mkcert.sh /mkcert.sh
-cp /install/dovecot/dovecot-openssl.cnf /dovecot-openssl.cnf
-sh /install/dovecot/mkcert.sh
 cp /install/dovecot/conf.d/10-ssl.conf /etc/dovecot/conf.d/10-ssl.conf
